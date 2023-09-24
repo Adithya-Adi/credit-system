89ed8fbf643914bcdfcf4623891cd175b7f11f31
Adithya <adithyahebbar32@gmail.com>
2023-09-24 10:42:40 +0530

    removed unwanted imports

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -1,7 +1,7 @@
 
 
 // reactstrap components
-import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+// import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 
 const Header = () => {
   return (
