eba735bf1aa85a208934c9d764a17385805e737b
Adithya <adithyahebbar32@gmail.com>
2023-09-30 20:18:32 +0530

    fixed baseurl

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // baseUrl: "http://localhost:8000",
-  baseUrl: "https://shrivaishnavifuels-1.onrender.com",
+  baseUrl: "https://shrivaishnavifuels.onrender.com",
 };
