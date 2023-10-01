7a82ee027b4acc72a4b10b8bde4a9f6a60431a0e
Adithya <adithyahebbar32@gmail.com>
2023-10-01 11:54:29 +0530

    changed backend baseurl

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: "http://localhost:8000",
-  // baseUrl: "https://shrivaishnavifuels.onrender.com",
+  // baseUrl: "http://localhost:8000",
+  baseUrl: "https://vaishnavifuels.onrender.com",
 };
